b25fe98b6404263a8cf891adb8b58fbea7445209
adamhinckley <adamhinckley@mac.com>
2019-03-22 10:29:47 -0600

    MVP done

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,14 +1,19 @@
 **1. List all of the main states a process may be in at any point in time on a standard Unix system. Briefly explain what each of these states means.**
 
-
+-   Running - this is the state when it's running
+-   Sleeping - when it's done running it sleeps
+-   Zombie - when a child has finished it's process, but has not reported to the parent yet
+-   Stopped - parent adopts the zombie and stops the process
 
 **2. What is a zombie process?**
 
-
+-   A child process that has finished it's job, but has not yet been stopped by the parent
 
 **3. How does a zombie process get created? How does one get destroyed?**
 
-
+-   It's created when a child it exits it's process
+-   It's destroyed when it connects to the parent
 
 **4. What are some of the benefits of working in a compiled language versus a non-compiled language? More specifically, what benefits are there to be had from taking the extra time to compile our code?**
 
+`Compiled languages run faster because they are converted into machine language and run "closer to the metal"`
--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,10 +10,49 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path;
+  // Check for empty command line
+  if (argc > 1)
+  {
+    path = argv[1];
+  }
+  else
+  {
+    path = ".";
+  }
 
   // Open directory
+  DIR *directory = opendir(path);
+  // Check if directory could not be opened.
+  if (directory == NULL)
+  {
+    printf("Directory could not be opened: %s\n", path);
+    exit(1);
+  }
 
   // Repeatly read and print entries
+  struct dirent *ent;
+  struct stat buf;
+  char *fp;
+
+  while ((ent = readdir(directory)) != NULL)
+  {
+    fp = malloc(strlen(path) + strlen(ent->d_name) + 1);
+
+    if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0)
+    {
+      sprintf(fp, "%s/%s", path, ent->d_name);
+    }
+    else
+    {
+      sprintf(fp, "%s", ent->d_name);
+    }
+
+    stat(fp, &buf);
+
+    printf("%10ld %s\n", buf.st_size, ent->d_name);
+    free(fp);
+  }
 
   // Close directory
 
